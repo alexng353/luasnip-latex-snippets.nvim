bf5b5a8ebd2de98f057a2eaccaa540f43bb634b0
Alexander Ng <alexng353@gmail.com>
2025-03-26 13:35:19 -0700

    bonk

--- lua/luasnip-latex-snippets/luasnippets/tex/math.lua
+++ lua/luasnip-latex-snippets/luasnippets/tex/math.lua
@@ -149,13 +149,23 @@ M = {
       { c(1, { t("*"), t(""), t("ed") }), i(2), rep(1) }),
     { condition = line_begin, show_condition = line_begin }),
 
-  autosnippet({ trig = "eqn", name = "equation(|*)", dscr = "equation math" },
+  autosnippet({ trig = "eq*", name = "equation*", dscr = "equation" },
     fmta([[
-    \begin{equation<>}
+    \begin{equation*}
+      <>
+    .\end{equation*}
     <>
-    .\end{equation<>}
     ]],
-      { c(1, { t("*"), t("") }), i(2), rep(1) }),
+      { i(1), i(0) }),
+    { condition = line_begin, show_condition = line_begin }),
+  autosnippet({ trig = "eqn", name = "equation", dscr = "equation" },
+    fmta([[
+    \begin{equation}
+      <>
+    .\end{equation}
+    <>
+    ]],
+      { i(1), i(0) }),
     { condition = line_begin, show_condition = line_begin }),
 
   -- Matrices and Cases
